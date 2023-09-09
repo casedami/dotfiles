807180d51e0f457428c3cba4b28895d1df929385
Casey Miller <cd.miller1230@gmail.com>
2023-09-09 17:36:27 -0600

    changed branch color

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -16,7 +16,7 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {{'branch', icon = '', padding = {left=2}}, 'diff', 'diagnostics'},
+    lualine_b = {{'branch', icon = '', padding = {left = 2}, color = {fg = '#f5e3b5'}, use_mode_colors = false}, 'diff', 'diagnostics'},
     lualine_c = {'%f'},
     lualine_x = {'filetype'},
     lualine_y = {'location'},
