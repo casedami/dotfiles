dbb9695f7425ee212e674c79fa607c281c1893a2
Casey Miller <cd.miller1230@gmail.com>
2023-09-10 14:45:25 -0600

    disabled some things, fixing c support

--- nvim/lua/plugins/servers.lua
+++ nvim/lua/plugins/servers.lua
@@ -1,10 +1,10 @@
-local util = require("util")
-
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      util.list_insert_unique(opts.ensure_installed, { "c", "cpp" })
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "c", "cpp", "python" })
+      end
     end,
   },
   {
@@ -12,28 +12,69 @@ return {
     opts = {
       servers = {
         pyright = {},
-        clangd = {},
+        clangd = {
+          keys = {
+            { "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+          },
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "Makefile",
+              "configure.ac",
+              "configure.in",
+              "config.h.in",
+              "meson.build",
+              "meson_options.txt",
+              "build.ninja"
+            )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+              fname
+            ) or require("lspconfig.util").find_git_ancestor(fname)
+          end,
+          capabilities = {
+            offsetEncoding = { "utf-16" },
+          },
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
+          },
+        },
+        setup = {
+          clangd = function(_, opts)
+            local clangd_ext_opts = require("lazyvim.util").opts("clangd_extensions.nvim")
+            require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+            return false
+          end,
+        },
       },
     },
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "pyright",
-        "black",
-        "clangd",
-        "clang-format",
-      })
-    end,
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = vim.list_extend(opts.sources, {
-        nls.builtins.formatting.black,
-      })
-    end,
+    {
+      "williamboman/mason.nvim",
+      opts = function(_, opts)
+        vim.list_extend(opts.ensure_installed, {
+          "pyright",
+          "black",
+          "clangd",
+          "clang-format",
+        })
+      end,
+    },
+    {
+      "jose-elias-alvarez/null-ls.nvim",
+      opts = function(_, opts)
+        local nls = require("null-ls")
+        opts.sources = vim.list_extend(opts.sources, {
+          nls.builtins.formatting.black,
+        })
+      end,
+    },
   },
 }
