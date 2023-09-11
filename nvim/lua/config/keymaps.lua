572b6e7d99f2bae87214e875e7248654574c3c56
Casey Miller <cd.miller1230@gmail.com>
2023-09-10 20:06:02 -0600

    remove lazygit

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
 
 vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv", { desc = "Move down", silent = true })
 vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move up", silent = true })
+vim.keymap.set("n", "<leader>gg", "<nop>")
+vim.keymap.set("n", "<leader>gG", "<nop>")
--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,8 @@
 return {
   { "stevearc/dressing.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "folke/trouble.nvim", enabled = false },
+  { "SmiteshP/nvim-navic", enabled = false },
+  { "jesseduffield/lazygit", enabled = false },
 }
