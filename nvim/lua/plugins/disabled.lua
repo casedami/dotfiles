25ef2bb98ba2b1f6ae7ae583f28c7aee83dbef71
Casey Miller <cd.miller1230@gmail.com>
2023-09-11 18:29:50 -0600

    renaming

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,7 +1,6 @@
 return {
   { "stevearc/dressing.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
-  { "rafamadriz/friendly-snippets", enabled = false },
   { "folke/trouble.nvim", enabled = false },
   { "SmiteshP/nvim-navic", enabled = false },
   { "jesseduffield/lazygit", enabled = false },
