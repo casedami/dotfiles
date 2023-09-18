bdecc031a91bc66b447d47a91280b24052ffb02f
Casey Miller <cd.miller1230@gmail.com>
2023-09-17 18:43:00 -0600

    adding more snippets

--- nvim/snippets/tex.lua
+++ nvim/snippets/tex.lua
@@ -18,29 +18,47 @@ return {
       ]],
       {
         i(1, "env"),
-        i(0, "body"),
+        i(0),
         rep(1),
       }
     ),
-    { condition = line_begin }
+    {}
   ),
   s(
     "tikz",
     fmta(
       [[
-          \begin{tikzpicture}[baseline]
-            \node[state, initial] (q0) {$q_0$};
-            \node[state, right of=q0] (q1) {$q_1$};
+        \begin{tikzpicture}[baseline]
+          <>
+        \end{tikzpicture}
+      ]],
+      {
+        i(0),
+      }
+    ),
+    {}
+  ),
+  s(
+    "fsm",
+    fmta(
+      [[
+        \begin{tikzpicture}[baseline]
+          \node[state, initial] (q0) {$q_0$};
+          \node[state, right of=q0] (q1) {$q_1$};
 
-            \draw
-            (q0) edge [above] node {<>};
+          \draw
+          (q0) edge [above] node {<>};
 
-          \end{tikzpicture}
-        ]],
+        \end{tikzpicture}
+      ]],
       {
         i(0, "transition"),
       }
     ),
-    { condition = line_begin }
+    {}
   ),
+  s({ trig = "mm", dscr = "inline math" }, fmta("\\( <> \\)", { i(1) }), {}),
+  s({ trig = "bf", dscr = "boldface" }, fmta("\\textbf{<>}", { i(1) }), {}),
+  s({ trig = "if", dscr = "italics" }, fmta("\\textit{<>}", { i(1) }), {}),
+  s({ trig = "setdef", dscr = "set definition" }, fmta("\\( \\{<>\\} \\)", { i(1) }), {}),
 }
