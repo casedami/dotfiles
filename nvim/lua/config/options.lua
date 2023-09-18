eac1499e51f1a2b4ccc95abba4afa36119c1963c
Casey Miller <cd.miller1230@gmail.com>
2023-09-18 14:44:44 -0600

    change to lualatex

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -38,7 +38,7 @@ vim.g.vimtex_view_skim_activate = 1
 vim.g.vimtex_indent_on = 1
 vim.g.vimtex_syntax_conceal_disable = 1
 vim.g.vimtex_quickfix_open_on_warning = 0
-vim.g.vimtex_compiler_latexmk_engines = { ["_"] = "-xelatex" }
+vim.g.vimtex_compiler_latexmk_engines = { ["_"] = "-lualatex" }
 vim.g.vimtex_compiler_method = "latexmk"
 vim.g.vimtex_compiler_latexmk = {
   callback = 1,
