e41b87bb0751a0f360f885b999b085a82ca11878
Casey Miller <cd.miller1230@gmail.com>
2023-11-22 10:07:50 -0700

    adding gitsigns

--- nvim/lua/cmill/plugins/coding.lua
+++ nvim/lua/cmill/plugins/coding.lua
@@ -31,4 +31,11 @@ return {
       },
     },
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufRead", "BufNewFile" },
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
 }
