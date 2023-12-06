5005b0f714faef5a720e35c64e7cbb2bf89fb50d
Casey Miller <cd.miller1230@gmail.com>
2023-12-05 18:55:48 -0700

    updating gitsigns maps

--- nvim/lua/cmill/plugins/coding.lua
+++ nvim/lua/cmill/plugins/coding.lua
@@ -35,7 +35,60 @@ return {
     "lewis6991/gitsigns.nvim",
     event = { "BufRead", "BufNewFile" },
     config = function()
-      require("gitsigns").setup()
+      require("gitsigns").setup({
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- aavigation
+          map("n", "[h", function()
+            if vim.wo.diff then
+              return "[h"
+            end
+            vim.schedule(function()
+              gs.next_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          map("n", "]h", function()
+            if vim.wo.diff then
+              return "]h"
+            end
+            vim.schedule(function()
+              gs.prev_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          -- actions
+          map("n", "<localleader>gs", gs.stage_hunk)
+          map("n", "<localleader>gr", gs.reset_hunk)
+          map("v", "<localleader>gs", function()
+            gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          end)
+          map("v", "<localleader>gr", function()
+            gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          end)
+          map("n", "<localleader>gS", gs.stage_buffer)
+          map("n", "<localleader>gu", gs.undo_stage_hunk)
+          map("n", "<localleader>gR", gs.reset_buffer)
+          map("n", "<localleader>gp", gs.preview_hunk)
+          map("n", "<localleader>gb", function()
+            gs.blame_line({ full = true })
+          end)
+          map("n", "<localleader>gd", gs.diffthis)
+          map("n", "<localleader>gD", function()
+            gs.diffthis("~")
+          end)
+          map("n", "<localleader>td", gs.toggle_deleted)
+        end,
+      })
     end,
   },
 }
--- nvim/lua/cmill/plugins/util.lua
+++ nvim/lua/cmill/plugins/util.lua
@@ -17,12 +17,6 @@ return {
   },
   {
     "tpope/vim-fugitive",
-    event = "BufRead",
-    keys = {
-      { "<localleader>gs", "<cmd>Git status<cr>" },
-      { "<localleader>gl", "<cmd>Git log1<cr>" },
-      { "<localleader>gL", "<cmd>Git glog<cr>" },
-    },
     cmd = "G",
   },
 }
