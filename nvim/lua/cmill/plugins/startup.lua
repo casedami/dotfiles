166e01dd413ec1d58bae5d3ee1e6c3ff5832c374
Casey Miller <cd.miller1230@gmail.com>
2024-01-07 15:35:45 -0700

    feat: file explorer opt on startup

--- nvim/lua/cmill/plugins/startup.lua
+++ nvim/lua/cmill/plugins/startup.lua
@@ -23,8 +23,9 @@ return {
         -- stylua: ignore
         center = {
           { action = "Telescope find_files",              desc = " Find file",       icon = " ", key = "f" },
-          { action = "Telescope oldfiles",                desc = " Recent files",    icon = " ", key = "r" },
+          { action = "Explore",                           desc = " File Explorer",   icon = " ", key = "e" },
           { action = "Telescope live_grep",               desc = " Find text",       icon = " ", key = "g" },
+          { action = "Telescope oldfiles",                desc = " Recent files",    icon = " ", key = "r" },
           { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
           { action = "Lazy",                              desc = " Lazy",            icon = "󰒲 ", key = "l" },
           { action = "qa",                                desc = " Quit",            icon = " ", key = "q" },
