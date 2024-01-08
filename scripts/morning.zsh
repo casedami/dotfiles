285b5900c49ecda967916820b017e934573d7568
Casey Miller <cd.miller1230@gmail.com>
2024-01-08 10:06:42 -0700

    incompl: adjusting script

--- scripts/morning.zsh
+++ scripts/morning.zsh
@@ -1,2 +1,5 @@
 open "obsidian://open?vault=uni&file=tasks%2Funi"
-nvim -o2 /Users/caseymiller/self/uni/tasks/self.md /Users/caseymiller/self/uni/morning.md
+sleep 1
+yabai -m space --rotate 270
+nvim -O2 /Users/caseymiller/self/uni/tasks/self.md /Users/caseymiller/self/uni/morning.md
+# yabai -m window --focus north
