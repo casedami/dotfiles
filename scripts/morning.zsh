bfd5bed8b4d63e9ae619b0cc1780dd652718b0b8
Casey Miller <cd.miller1230@gmail.com>
2024-01-08 21:19:27 -0700

    fix: swapping layout

--- scripts/morning.zsh
+++ scripts/morning.zsh
@@ -2,4 +2,4 @@ open "obsidian://open?vault=uni&file=tasks%2Funi"
 sleep 1
 yabai -m space --rotate 270
 yabai -m window --focus north
-nvim -O2 /Users/caseymiller/self/uni/tasks/self.md /Users/caseymiller/self/uni/morning.md
+nvim -O2 /Users/caseymiller/self/uni/morning.md /Users/caseymiller/self/uni/tasks/self.md
