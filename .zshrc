9bf90757cc928f9a51f8b605f6ccaf78a21728d6
Casey Miller <cd.miller1230@gmail.com>
2024-01-11 21:01:26 -0700

    feat: removing p10k, setting up prompt

--- .zshrc
+++ .zshrc
@@ -1,20 +1,9 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
-# load prompt plugin
-fpath+=("$(brew --prefix)/share/zsh/site-functions")
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
 PATH="$PATH:$HOME/.local/bin/scripts/"
 PATH="$PATH:$HOME/Library/Python/3.11/bin"
 
 [ -f "/Users/caseymiller/.ghcup/env" ] && source "/Users/caseymiller/.ghcup/env" # ghcup-env
 
-source /opt/homebrew/opt/powerlevel10k/powerlevel10k.zsh-theme
+source /Users/caseymiller/.config/zsh/prompt.zsh
 source /Users/caseymiller/.config/zsh/sources.sh
 source /Users/caseymiller/.config/zsh/opt.sh
 source /Users/caseymiller/.config/zsh/fg_bg.zsh
---
+++ zsh/prompt.zsh
@@ -0,0 +1,57 @@
+autoload -Uz vcs_info
+# autoload -Uz promptinit && promptinit
+setopt PROMPT_SUBST
+
+# print new line after each entry
+precmd() {
+  precmd() {
+    echo
+  }
+}
+
+precmd() {
+  vcs_info
+}
+
+precmd_functions+=( precmd_vcs_info )
+
+# change promptchar depending on current mode
+ins="%#"
+com=">"
+mode=$ins
+
+function zle-keymap-select {
+  mode="${${KEYMAP/vicmd/${com}}/(main|viins)/${ins}}"
+  zle reset-prompt
+}
+zle -N zle-keymap-select
+
+function zle-line-finish {
+  mode=$ins
+}
+zle -N zle-line-finish
+
+function TRAPINT() {
+  mode=$ins
+  return $(( 128 + $1 ))
+}
+
+# build prompt
+dir="%F{blue}%(3~|.../%1~|%~)%f "
+git='${vcs_info_msg_0_}'
+promptchar='%(?|%F{white}${mode}%f |%F{red}${mode}%f '
+
+PS1=${PS1//$prompt_newline/$'\n'}
+ps1=( ${(f)PS1} )
+ps1=(
+  $dir
+  $git
+  $promptchar
+)
+PS1="${(j::)ps1}"
+
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' unstagedstr "*"
+zstyle ':vcs_info:*' stagedstr ""
+zstyle ':vcs_info:git:*' formats '%F{yellow}%b%u%c%f '
