344350bc60145c4bd854c22b01ed804f52070b0e
Casey Miller <cd.miller1230@gmail.com>
2024-01-11 21:24:24 -0700

    fix: git info

--- .zshrc
+++ .zshrc
@@ -4,7 +4,7 @@ PATH="$PATH:$HOME/Library/Python/3.11/bin"
 [ -f "/Users/caseymiller/.ghcup/env" ] && source "/Users/caseymiller/.ghcup/env" # ghcup-env
 
 source /Users/caseymiller/.config/zsh/prompt.zsh
-source /Users/caseymiller/.config/zsh/sources.sh
+source /Users/caseymiller/.config/zsh/aliases.sh
 source /Users/caseymiller/.config/zsh/opt.sh
 source /Users/caseymiller/.config/zsh/fg_bg.zsh
 source /Users/caseymiller/.config/zsh/fzf.sh
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -50,8 +50,29 @@ ps1=(
 )
 PS1="${(j::)ps1}"
 
+zstyle ':vcs_info:git*+set-message:*' hooks git-st
+function +vi-git-st() {
+    local ahead behind
+    local -a gitstatus
+
+    # Exit early in case the worktree is on a detached HEAD
+    git rev-parse ${hook_com[branch]}@{upstream} >/dev/null 2>&1 || return 0
+
+    local -a ahead_and_behind=(
+        $(git rev-list --left-right --count HEAD...${hook_com[branch]}@{upstream} 2>/dev/null)
+    )
+
+    ahead=${ahead_and_behind[1]}
+    behind=${ahead_and_behind[2]}
+
+    (( $ahead )) && gitstatus+=( "+${ahead}" )
+    (( $behind )) && gitstatus+=( "-${behind}" )
+
+    hook_com[misc]+=${(j:/:)gitstatus}
+}
+
 zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' unstagedstr "*"
 zstyle ':vcs_info:*' stagedstr ""
-zstyle ':vcs_info:git:*' formats '%F{yellow}%b%u%f %F{cyan}%c%f (%a) '
+zstyle ':vcs_info:git:*' formats '%F{yellow}%b%u%f %F{cyan}%c%m%f '
