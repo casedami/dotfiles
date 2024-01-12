5c2bb0d1ae8d9d7266eccd54fe6b4786ee46c0b9
Casey Miller <cd.miller1230@gmail.com>
2024-01-12 09:56:40 -0700

    fix: hooks for newline

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,19 +1,51 @@
 autoload -Uz vcs_info
-autoload -Uz promptinit && promptinit
+autoload -Uz add-zsh-hook
+precmd_functions+=( precmd_vcs_info )
 setopt PROMPT_SUBST
 
-# print new line after each entry
-precmd() {
-  precmd() {
-    echo
-  }
-}
-
-precmd() {
+load_vcs () {
   vcs_info
 }
 
-precmd_functions+=( precmd_vcs_info )
+# print newline after each command
+newline() {
+  precmd() {
+      echo
+  }
+}
+
+add-zsh-hook precmd load_vcs
+add-zsh-hook precmd newline
+
+# don't print newline before prompt on clear command
+alias clear="precmd() {precmd() {echo }} && clear"
+
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' unstagedstr "*"
+zstyle ':vcs_info:*' stagedstr "+"
+zstyle ':vcs_info:git:*' formats '%F{yellow}%b%u%f %F{cyan}%c%m%f'
+
+zstyle ':vcs_info:git*+set-message:*' hooks git-st
+function +vi-git-st() {
+    local ahead behind
+    local -a gitstatus
+
+    # exit early in case the worktree is on a detached HEAD
+    git rev-parse ${hook_com[branch]}@{upstream} >/dev/null 2>&1 || return 0
+
+    local -a ahead_and_behind=(
+        $(git rev-list --left-right --count HEAD...${hook_com[branch]}@{upstream} 2>/dev/null)
+    )
+
+    ahead=${ahead_and_behind[1]}
+    behind=${ahead_and_behind[2]}
+
+    (( $ahead )) && gitstatus+=( "" )
+    (( $behind )) && gitstatus+=( "" )
+
+    hook_com[misc]+=${(j:/:)gitstatus}
+}
 
 # change promptchar depending on current mode
 ins="%#"
@@ -41,38 +73,9 @@ dir="%F{blue}%(3~|.../%1~|%~)%f "
 git='${vcs_info_msg_0_}'
 promptchar='%(?|%F{white}${mode}%f |%F{red}${mode}%f '
 
-PS1=${PS1//$prompt_newline/$'\n'}
-ps1=( ${(f)PS1} )
 ps1=(
   $dir
   $git
   $promptchar
 )
 PS1="${(j::)ps1}"
-
-zstyle ':vcs_info:git*+set-message:*' hooks git-st
-function +vi-git-st() {
-    local ahead behind
-    local -a gitstatus
-
-    # Exit early in case the worktree is on a detached HEAD
-    git rev-parse ${hook_com[branch]}@{upstream} >/dev/null 2>&1 || return 0
-
-    local -a ahead_and_behind=(
-        $(git rev-list --left-right --count HEAD...${hook_com[branch]}@{upstream} 2>/dev/null)
-    )
-
-    ahead=${ahead_and_behind[1]}
-    behind=${ahead_and_behind[2]}
-
-    (( $ahead )) && gitstatus+=( "+${ahead}" )
-    (( $behind )) && gitstatus+=( "-${behind}" )
-
-    hook_com[misc]+=${(j:/:)gitstatus}
-}
-
-zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' unstagedstr "*"
-zstyle ':vcs_info:*' stagedstr ""
-zstyle ':vcs_info:git:*' formats '%F{yellow}%b%u%f %F{cyan}%c%m%f '
