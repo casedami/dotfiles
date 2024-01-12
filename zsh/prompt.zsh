0ff2f318ac64b8e10bf5dc627959870624aefc16
Casey Miller <cd.miller1230@gmail.com>
2024-01-12 10:01:50 -0700

    fix: spacing

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -24,7 +24,7 @@ zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' unstagedstr "*"
 zstyle ':vcs_info:*' stagedstr "+"
-zstyle ':vcs_info:git:*' formats '%F{yellow}%b%u%f %F{cyan}%c%m%f'
+zstyle ':vcs_info:git:*' formats '%F{yellow}%b%u%f%F{cyan}%c%m%f '
 
 zstyle ':vcs_info:git*+set-message:*' hooks git-st
 function +vi-git-st() {
