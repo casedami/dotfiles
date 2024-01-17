2697b8d48cdf3b9e6add6d5fc9a3b55f992d4824
Casey Miller <cd.miller1230@gmail.com>
2024-01-17 15:55:28 -0700

    fix: removing dev opts

--- zsh/opt.sh
+++ zsh/opt.sh
@@ -14,7 +14,5 @@ setopt extended_glob # extened globbing functionality
 setopt vi            # turn on vi-mode
 
 export LANG="en_US.UTF-8"
-export LC_ALL="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
 export TERM="xterm-256color"
 eval "$(zoxide init zsh)"
