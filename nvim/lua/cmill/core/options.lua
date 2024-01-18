c24c7185913f6b1288897b3f0b1f78bfad728244
Casey Miller <cd.miller1230@gmail.com>
2024-01-17 17:20:19 -0700

    refactor: cleanup opts

--- nvim/lua/cmill/core/options.lua
+++ nvim/lua/cmill/core/options.lua
@@ -1,98 +1,121 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+local global_opts = {
+  -- LEADER KEYS   =======================================
+  mapleader = " ",
+  maplocalleader = "\\",
 
-vim.g.autoformat = true
-vim.g.netrw_banner = 0
-vim.g.netrw_browse_split = 0
-vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
+  -- NETRW   =============================================
+  netrw_banner = 0,
+  netrw_browse_split = 0,
+  netrw_bufsettings = "noma nomod nu nobl nowrap ro",
 
-local opt = vim.opt
+  -- MARKDON   ===========================================
+  autoformat = true,
+  markdown_recommended_style = 0,
+  markdown_folding = 1,
 
-opt.autowrite = true -- enable auto write
-opt.background = "dark"
-opt.clipboard = "unnamedplus" -- sync with system clipboard
-opt.colorcolumn = ""
-opt.completeopt = "menu,menuone,noselect"
-opt.conceallevel = 2 -- hide * markup for bold and italic
-opt.confirm = true -- confirm to save changes before exiting modified buffer
-opt.cursorline = true -- enable highlighting of the current line
-opt.cursorlineopt = "number"
-opt.expandtab = true -- use spaces instead of tabs
-opt.formatoptions = "jcroqlnt" -- tcqj
-opt.foldcolumn = "auto"
-opt.foldmethod = "indent"
-opt.foldenable = false
-opt.foldlevel = 99
-opt.grepformat = "%f:%l:%c:%m"
-opt.grepprg = "rg --vimgrep"
-opt.ignorecase = true -- ignore case
-opt.inccommand = "nosplit" -- preview incremental substitute
-opt.laststatus = 3 -- global statusline
-opt.list = true -- show some invisible characters (tabs...
-opt.number = true -- show current line number
-opt.pumblend = 10 -- popup blend
-opt.pumheight = 10 -- maximum number of entries in a popup
-opt.relativenumber = true -- relative line numbers
-opt.ruler = false
-opt.scrolloff = 4 -- lines of context
-opt.sessionoptions =
-  { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
-opt.shiftround = true -- round indent
-opt.shiftwidth = 2 -- size of an indent
-opt.shortmess:append({ W = true, I = true, c = true, C = true })
-opt.showmode = false -- dont show mode since we have a statusline
-opt.showtabline = 0
-opt.sidescrolloff = 8 -- columns of context
-opt.signcolumn = "yes:1" -- always show the signcolumn, otherwise it would shift the text each time
-opt.smartcase = true -- don't ignore case with capitals
-opt.smartindent = true -- insert indents automatically
-opt.spelllang = { "en" }
-opt.splitbelow = true -- put new windows below current
-opt.splitkeep = "screen"
-opt.splitright = true -- put new windows right of current
-opt.statuscolumn = "%!v:lua.require'cmill.core.util'.statuscolumn()"
-opt.tabstop = 2 -- number of spaces tabs count for
-opt.termguicolors = true -- true color support
-opt.timeoutlen = 300
-opt.undofile = true
-opt.undolevels = 10000
-opt.updatetime = 200 -- save swap file and trigger cursorhold
-opt.virtualedit = "block" -- allow cursor to move where there is no text in visual block mode
-opt.wildmode = "longest:full,full" -- command-line completion mode
-opt.winminwidth = 5 -- minimum window width
-opt.wrap = false -- disable line wrap
-opt.fillchars = {
-  foldopen = "",
-  foldclose = "",
-  foldsep = " ",
-  fold = " ",
-  diff = "╱",
-  eob = "~",
-}
-
--- fix markdown indentation settings
-vim.g.markdown_recommended_style = 0
-vim.g.markdown_folding = 1
-
--- vimtex options
-vim.g.tex_flavor = "latex"
-vim.g.vimtex_view_method = "skim"
-vim.g.vimtex_view_skim_sync = 1
-vim.g.vimtex_view_skim_activate = 1
-vim.g.vimtex_indent_on = 1
-vim.g.vimtex_syntax_conceal_disable = 1
-vim.g.vimtex_quickfix_open_on_warning = 0
-vim.g.vimtex_compiler_latexmk_engines = { ["_"] = "-lualatex" }
-vim.g.vimtex_compiler_method = "latexmk"
-vim.g.vimtex_compiler_latexmk = {
-  callback = 1,
-  continuous = 1,
-  executable = "latexmk",
-  options = {
-    "-shell-escape",
-    "-verbose",
-    "-file-line-error",
-    "-synctex=1",
-    "-interaction=nonstopmode",
+  -- TEX   ===============================================
+  tex_flavor = "latex",
+  vimtex_view_method = "skim",
+  vimtex_view_skim_sync = 1,
+  vimtex_view_skim_activate = 1,
+  vimtex_indent_on = 1,
+  vimtex_syntax_conceal_disable = 1,
+  vimtex_quickfix_open_on_warning = 0,
+  vimtex_compiler_latexmk_engines = { ["_"] = "-lualatex" },
+  vimtex_compiler_method = "latexmk",
+  vimtex_compiler_latexmk = {
+    callback = 1,
+    continuous = 1,
+    executable = "latexmk",
+    options = {
+      "-shell-escape",
+      "-verbose",
+      "-file-line-error",
+      "-synctex=1",
+      "-interaction=nonstopmode",
+    },
   },
 }
+
+local options = {
+  -- TEXT   ==============================================
+  fillchars = {
+    foldopen = "",
+    foldclose = "",
+    foldsep = " ",
+    fold = " ",
+    diff = "╱",
+    eob = "~",
+  },
+  list = true, -- show some invisible characters (tabs...
+  scrolloff = 4, -- lines of context
+  shiftround = true,
+  shiftwidth = 2, -- size of an indent
+  spelllang = { "en" },
+  tabstop = 2, -- number of spaces tabs count for
+  wrap = false,
+  -- UX   ================================================
+  autowrite = true,
+  clipboard = "unnamedplus", -- sync with system clipboard
+  completeopt = "menu,menuone,noselect",
+  confirm = true, -- confirm to save changes before exiting modified buffer
+  expandtab = true, -- use spaces instead of tabs
+  foldcolumn = "auto",
+  foldmethod = "indent",
+  foldenable = false,
+  foldlevel = 99,
+  ignorecase = true,
+  inccommand = "nosplit", -- preview incremental substitute
+  smartcase = true,
+  smartindent = true, -- insert indents automatically
+  splitbelow = true, -- put new windows below current
+  splitkeep = "screen",
+  splitright = true, -- put new windows right of current
+  timeoutlen = 300,
+  undofile = true,
+  undolevels = 10000,
+  virtualedit = "block", -- allow cursor to move where there is no text in visual block mode
+  wildmode = "longest:full,full", -- command-line completion mode
+  -- UI   ================================================
+  background = "dark",
+  colorcolumn = "",
+  conceallevel = 2, -- hide * markup for bold and italic
+  cursorline = true,
+  cursorlineopt = "number",
+  laststatus = 3, -- global statusline
+  number = true, -- show current line number
+  relativenumber = true, -- relative line numbers
+  pumblend = 10, -- popup blend
+  pumheight = 10, -- maximum number of entries in a popup
+  ruler = false,
+  showmode = false,
+  showtabline = 0,
+  sidescrolloff = 8, -- columns of context
+  signcolumn = "yes:1", -- always show the signcolumn, otherwise it would shift the text each time
+  statuscolumn = "%!v:lua.require'cmill.core.util'.statuscolumn()",
+  termguicolors = true,
+  updatetime = 200,
+  winminwidth = 5,
+  -- MISC   ==============================================
+  formatoptions = "jcroqlnt",
+  grepformat = "%f:%l:%c:%m",
+  grepprg = "rg, --vimgrep",
+  sessionoptions = {
+    "buffers",
+    "curdir",
+    "tabpages",
+    "winsize",
+    "help",
+    "globals",
+    "skiprtp",
+    "folds",
+  },
+}
+
+for k, v in pairs(global_opts) do
+  vim.g[k] = v
+end
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
