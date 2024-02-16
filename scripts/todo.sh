aaea429eb3d10fd44d05edad18050db7182411be
Casey Miller <cd.miller1230@gmail.com>
2024-02-16 13:06:46 -0700

    adjusting keymaps/spacing

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -7,7 +7,7 @@ if [[ "$1" == "new" ]]; then
   task=$(echo $task | xargs)
   echo "- [ ] $task" >>~/self/notes/main/tasks/self.md
 elif [[ "$1" == "ls" ]]; then
-  cat ~/self/notes/main/tasks/self.md | sed 's/\[ \] //' | sed '1d'
+  cat ~/self/notes/main/tasks/self.md | sed 's/\[ \] //' | sed '1d;2d'
 else
   echo "unsupported or unknown args"
 fi
