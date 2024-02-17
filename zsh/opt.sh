7faac15fca127fe3e018c747d1208a79ea6565f4
Casey Miller <cd.miller1230@gmail.com>
2024-02-17 14:55:56 -0700

    changing fzf opts

--- zsh/opt.sh
+++ zsh/opt.sh
@@ -17,11 +17,14 @@ export LANG="en_US.UTF-8"
 export TERM="xterm-256color"
 eval "$(zoxide init zsh)"
 
-export FZF_DEFAULT_OPTS="
---color=fg+:#91809e,pointer:#91809e,hl:#91809e,hl+:#91809e,gutter:-1,bg+:-1,prompt:#b7c3e3
---reverse
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+--color=fg+:#ecae6d,pointer:#ecae6d,hl:#ecae6d,hl+:#ecae6d,gutter:-1,bg+:-1,prompt:#bfce94,info:#4f5b58
+--separator="─" --scrollbar="│" --layout="reverse" --info="right"
+--prompt=" "
+--marker=">"
+--pointer="󱞪"
 --cycle
 --multi
 --height 40%
-"
+'
 export FZF_COMPLETION_TRIGGER=';'
