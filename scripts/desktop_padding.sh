49c67217eed7a30da7d422a799a1ce2f7eb0a203
Casey Miller <cd.miller1230@gmail.com>
2024-03-08 10:30:35 -0700

    update; fix highlights; fix script on login

--- scripts/desktop_padding.sh
+++ scripts/desktop_padding.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-desktop_spaces=$(yabai -m query --displays | jq "map(select(.id == 2).spaces)" | sed 's/[^0-9]*//g' | xargs)
+if [[ $(yabai -m query --displays | jq ".[].id" | grep 2) ]]; then
+  desktop_spaces=$(yabai -m query --displays | jq "map(select(.id == 2).spaces)" | sed 's/[^0-9]*//g' | xargs)
 
-if [[ $desktop_spaces == "" ]]; then
-  exit
+  for space in $desktop_spaces; do
+    yabai -m config --space $space top_padding 200
+    yabai -m config --space $space bottom_padding 200
+    yabai -m config --space $space left_padding 200
+    yabai -m config --space $space right_padding 200
+    yabai -m config --space $space window_gap 50
+  done
 fi
-
-for space in $desktop_spaces; do
-  yabai -m config --space $space top_padding 200
-  yabai -m config --space $space bottom_padding 200
-  yabai -m config --space $space left_padding 200
-  yabai -m config --space $space right_padding 200
-  yabai -m config --space $space window_gap 50
-done
