533aa1c279a4ec96fb134e9c47413c80b16bd763
Casey Miller <cd.miller1230@gmail.com>
2024-03-09 14:01:50 -0700

    fix: padding with >2 windows on ext monitor

--- scripts/desktop_padding.sh
+++ scripts/desktop_padding.sh
@@ -4,10 +4,17 @@ if [[ $(yabai -m query --displays | jq ".[].id" | grep 2) ]]; then
   desktop_spaces=$(yabai -m query --displays | jq "map(select(.id == 2).spaces)" | sed 's/[^0-9]*//g' | xargs)
 
   for space in $desktop_spaces; do
+    if [[ $(yabai -m query --windows --space $space | jq "length") > 2 ]]; then
+      hpad=50
+      gap=25
+    else
+      hpad=200
+      gap=50
+    fi
     yabai -m config --space $space top_padding 200
     yabai -m config --space $space bottom_padding 200
-    yabai -m config --space $space left_padding 200
-    yabai -m config --space $space right_padding 200
-    yabai -m config --space $space window_gap 50
+    yabai -m config --space $space left_padding $hpad
+    yabai -m config --space $space right_padding $hpad
+    yabai -m config --space $space window_gap $gap
   done
 fi
