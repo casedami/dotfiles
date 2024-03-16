0955ed8c859a524043919b755593ef781fc2e5d1
Casey Miller <cd.miller1230@gmail.com>
2024-03-16 14:11:39 -0600

    feat: clean, today, add multiple tasks

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -1,13 +1,44 @@
+#!/opt/homebrew/bin/bash
+
 if [[ $# -eq 0 ]]; then
+  nvim ~/self/notes/main/tasks/today.md
+  exit 0
+fi
+
+if [[ "$1" == "--self" ]]; then
   nvim ~/self/notes/main/tasks/self.md
   exit 0
 fi
+
+split_tasks() {
+  local input=$1
+  local file=$2
+  array=$(awk '{n = split($0, t, ", "); for (i = 0; ++i <= n;) print t[i]}' <<<"$input")
+  for item in $array; do
+    echo "- [ ] $item" >>$2
+  done
+}
+
 if [[ "$1" == "new" ]]; then
-  read -p ">>> " task
-  task=$(echo $task | xargs)
-  echo "- [ ] $task" >>~/self/notes/main/tasks/self.md
+  read -p ">>> " tasks
+  tasks=$(echo $tasks | xargs)
+  if [[ "$2" == "--self" ]]; then
+    split_tasks "$tasks" ~/self/notes/main/tasks/self.md
+  else
+    split_tasks "$tasks" ~/self/notes/main/tasks/today.md
+  fi
 elif [[ "$1" == "ls" ]]; then
-  cat ~/self/notes/main/tasks/self.md | grep -- '- \[ \]' | sed 's/\[ \] //'
+  if [[ "$2" == "--self" ]]; then
+    cat ~/self/notes/main/tasks/self.md | grep -- '- \[ \]' | sed 's/\[ \] //'
+  else
+    cat ~/self/notes/main/tasks/today.md | grep -E -- '- \[(x| )\]' | sed 's/- \[x\]//' | sed 's/\[ \] //' | sort
+  fi
+elif [[ "$1" == "clean" ]]; then
+  if [[ "$2" == "--self" ]]; then
+    sed -i -e '/- \[x\]/d' ~/self/notes/main/tasks/self.md
+  else
+    sed -i -e '/- \[x\]/d' ~/self/notes/main/tasks/today.md
+  fi
 else
   echo "unsupported or unknown args"
 fi
