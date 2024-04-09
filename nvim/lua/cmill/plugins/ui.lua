a23a1e18b325be87904b00b3b2da7d150a3fadb3
Casey Miller <cd.miller1230@gmail.com>
2024-04-09 11:53:40 -0600

    update; fix sep

--- nvim/lua/cmill/plugins/ui.lua
+++ nvim/lua/cmill/plugins/ui.lua
@@ -9,13 +9,14 @@ return {
   },
   {
     "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       local components = require("cmill.core.util").statusline_components()
       require("lualine").setup({
         options = {
           icons_enabled = true,
           theme = "auto",
-          section_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
           component_separators = { left = "", right = "" },
           disabled_filetypes = { "starter", "TelescopePrompt" },
           ignore_focus = {},
