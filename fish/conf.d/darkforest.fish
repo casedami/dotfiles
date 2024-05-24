1d1df6fd5751569930c6f37de56a4e20e28f8cec
Casey Miller <cd.miller1230@gmail.com>
2024-05-23 20:15:45 -0600

    update tm

--- fish/conf.d/darkforest.fish
+++ fish/conf.d/darkforest.fish
@@ -1,6 +1,10 @@
-set -U orange e69875 # color 16
-set -U alt d6a56f # color 17
+# DARKFOREST
+# use in ~/.config/fish/conf.d/
 
+set -g orange e69875 # color 16
+set -g alt d6a56f # color 17
+
+# Syntax Highlighting Colors
 set -g fish_color_normal bbbac1
 set -g fish_color_command 7da77e
 set -g fish_color_keyword 7da77e
@@ -13,11 +17,12 @@ set -g fish_color_comment 7a8478
 set -g fish_color_selection --background=313b35
 set -g fish_color_search_match --background=313b35
 set -g fish_color_operator abbceb
-set -g fish_color_escape 7da77e
+set -g fish_color_escape a7c080
 set -g fish_color_autosuggestion 7a8478
 
+# Completion Pager Colors}
 set -g fish_pager_color_progress 7a8478
-set -g fish_pager_color_prefix d6a56f
+set -g fish_pager_color_prefix 78b0a8
 set -g fish_pager_color_completion bbbac1
 set -g fish_pager_color_description 7a8478
 set -g fish_pager_color_selected_background --background=313b35
