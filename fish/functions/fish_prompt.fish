ba8fc6b68cad3ebcad711efdb6b6c5998adcf951
Casey Miller <cd.miller1230@gmail.com>
2024-05-27 22:06:04 -0600

    change colors

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,14 +1,20 @@
 function fish_prompt
     set -l __last_command_exit_status $status
     set -l magenta (set_color -o magenta)
+    set -l red (set_color -o red)
+    set -l brmagenta (set_color -o brmagenta)
 
-    set -g __fish_git_prompt_color $alt
     set -g __fish_git_prompt_showdirtystate 1
     set -g __fish_git_prompt_showupstream auto
-    set -g __fish_git_prompt_color_upstream magenta
+
+    set -g __fish_git_prompt_color $alt
     set -g __fish_git_prompt_color_prefix $orange
     set -g __fish_git_prompt_color_suffix $orange
+    set -g __fish_git_prompt_color_cleanstate brmagenta
     set -g __fish_git_prompt_color_dirtystate $orange
+    set -g __fish_git_prompt_color_upstream magenta
+    set -g __fish_git_prompt_color_merging red
+
     set -g __fish_git_prompt_char_upstream_ahead :↑
     set -g __fish_git_prompt_char_upstream_behind :↓
     set -g __fish_git_prompt_char_upstream_equal ''
