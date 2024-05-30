a01cf10f4fc5c35491a8a0ba56a3d781180d0988
Casey Miller <cd.miller1230@gmail.com>
2024-05-29 20:21:14 -0600

    rename

---
+++ fish/conf.d/theme.fish
@@ -0,0 +1,28 @@
+# ICECLIMBER
+# use in ~/.config/fish/conf.d/
+
+set -g orange d6a56f # color 16
+set -g alt abbceb # color 17
+
+# Syntax Highlighting Colors
+set -g fish_color_normal bbbac1
+set -g fish_color_command 817faf
+set -g fish_color_keyword 817faf
+set -g fish_color_quote 7da77e
+set -g fish_color_redirection e29eca
+set -g fish_color_end 817faf
+set -g fish_color_error e67e80
+set -g fish_color_param bbbac1
+set -g fish_color_comment 55555e
+set -g fish_color_selection --background=333336
+set -g fish_color_search_match --background=333336
+set -g fish_color_operator 9b99a3
+set -g fish_color_escape 8da0d6
+set -g fish_color_autosuggestion 55555e
+
+# Completion Pager Colors}
+set -g fish_pager_color_progress 55555e
+set -g fish_pager_color_prefix 559ba3
+set -g fish_pager_color_completion bbbac1
+set -g fish_pager_color_description 55555e
+set -g fish_pager_color_selected_background --background=333336
