a07ebde07f103c6001a96ed82e577ec5d68d094c
Casey Miller <cd.miller1230@gmail.com>
2024-07-26 16:22:58 -0600

    change back to iceclimber

--- fish/conf.d/theme.fish
+++ fish/conf.d/theme.fish
@@ -1,28 +1,28 @@
-#COFFEECAT
+# ICECLIMBER
 # use in ~/.config/fish/conf.d/
 
-set -g orange e3a968 # color 16
-set -g alt 9094de # color 17
+set -g orange d6a56f # color 16
+set -g alt abbceb # color 17
 
 # Syntax Highlighting Colors
 set -g fish_color_normal bbbac1
-set -g fish_color_command b39581
-set -g fish_color_keyword b39581
-set -g fish_color_quote 8fc490
-set -g fish_color_redirection 9094de
-set -g fish_color_end b39581
-set -g fish_color_error d47b7d
+set -g fish_color_command 8c8abd
+set -g fish_color_keyword 8c8abd
+set -g fish_color_quote 7da77e
+set -g fish_color_redirection e29eca
+set -g fish_color_end 8c8abd
+set -g fish_color_error e67e80
 set -g fish_color_param bbbac1
-set -g fish_color_comment 6e6a65
-set -g fish_color_selection --background=383737
-set -g fish_color_search_match --background=383737
+set -g fish_color_comment 55555e
+set -g fish_color_selection --background=333336
+set -g fish_color_search_match --background=333336
 set -g fish_color_operator 9b99a3
-set -g fish_color_escape abbceb
-set -g fish_color_autosuggestion 6e6a65
+set -g fish_color_escape 8da0d6
+set -g fish_color_autosuggestion 55555e
 
 # Completion Pager Colors}
-set -g fish_pager_color_progress 6e6a65
-set -g fish_pager_color_prefix cfa1bf
+set -g fish_pager_color_progress 55555e
+set -g fish_pager_color_prefix 559ba3
 set -g fish_pager_color_completion bbbac1
-set -g fish_pager_color_description 6e6a65
-set -g fish_pager_color_selected_background --background=383737
+set -g fish_pager_color_description 55555e
+set -g fish_pager_color_selected_background --background=333336
