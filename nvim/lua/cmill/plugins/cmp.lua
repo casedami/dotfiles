4667e87a6fe44afdb093fb215f8f2e54afc803db
Casey Miller <cd.miller1230@gmail.com>
2024-10-21 16:00:42 -0600

    small fixes

--- nvim/lua/cmill/plugins/cmp.lua
+++ nvim/lua/cmill/plugins/cmp.lua
@@ -22,14 +22,25 @@ return {
       local defaults = require("cmp.config.default")()
       local lspkind = require("lspkind")
       local cmp_window = require("cmp.config.window")
+      local maps = {
+        ["<C-n>"] = cmp.mapping.select_next_item({
+          behavior = cmp.SelectBehavior.Insert,
+        }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({
+          behavior = cmp.SelectBehavior.Insert,
+        }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-w>"] = cmp.mapping.abort(),
+        ["<tab>"] = cmp.mapping.confirm({ select = true }),
+      }
       cmp.setup.cmdline("/", {
-        mapping = cmp.mapping.preset.cmdline(),
+        mapping = maps,
         sources = {
           { name = "buffer" },
         },
       })
       cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
+        mapping = maps,
         sources = cmp.config.sources({
           { name = "path" },
         }, {
@@ -46,15 +57,7 @@ return {
           completeopt = "menu,menuone,noinsert",
         },
         -- stylua: ignore
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert, }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert, }),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-w>"] = cmp.mapping.abort(),
-          ["<tab>"] = cmp.mapping.confirm({ select = true }),
-        }),
+        mapping = cmp.mapping.preset.insert(maps),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "buffer" },
