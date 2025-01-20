a9fafba1e620736d34a4598cad59611789ea83e6
Casey Miller <cd.miller1230@gmail.com>
2025-01-19 19:04:17 -0700

    update and rename

--- nvim/lua/cmill/plugins/formatter.lua
+++ nvim/lua/cmill/plugins/formatter.lua
@@ -14,11 +14,11 @@ return {
       ["_"] = { "trim_whitespace" },
     },
     format_on_save = {
-      lsp_fallback = true,
+      lsp_fallback = false,
       timeout_ms = 500,
     },
     format_after_save = {
-      lsp_fallback = true,
+      lsp_fallback = false,
     },
     log_level = vim.log.levels.ERROR,
     notify_on_error = true,
@@ -28,6 +28,11 @@ return {
       },
       shfmt = {
         args = { "-i", "2" },
+      },
+      ruff_format = {
+        condition = function(_, ctx)
+          return not string.find(ctx.filename, "trident")
+        end,
       },
     },
   },
