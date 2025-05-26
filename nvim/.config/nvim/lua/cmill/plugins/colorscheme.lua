dab401c838d6f731fb918b6a8c01ac22a4b64766
Casey Miller <cd.miller1230@gmail.com>
2025-05-26 11:10:08 -0600

    styling updates, add hyprshot

--- nvim/.config/nvim/lua/cmill/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/cmill/plugins/colorscheme.lua
@@ -1,35 +1,37 @@
 return {
-	{
-		"cdmill/neomodern.nvim",
-		priority = 1000,
-		branch = "dev",
-		config = function()
-			require("neomodern").setup({
-				theme = "gyokuro",
-				toggle_variant_key = "<leader>uc",
-				alt_bg = false,
-				cursorline_gutter = false,
-				dark_gutter = false,
-				plain_float = true,
-				transparent = false,
-				code_style = {
-					headings = "italic",
-				},
-				plugin = {
-					lualine = {
-						plain = false,
-					},
-					cmp = {
-						plain = true,
-					},
-				},
-				highlights = {
-					["LazyNormal"] = { bg = "none" },
-					["Statusline"] = { bg = "$alt_bg" },
-					["StatuslineTerm"] = { bg = "$alt_bg" },
-				},
-			})
-			require("neomodern").load()
-		end,
-	},
+    {
+        "cdmill/neomodern.nvim",
+        priority = 1000,
+        branch = "dev",
+        config = function()
+            require("neomodern").setup({
+                theme = "gyokuro",
+                toggle_variant_key = "<leader>uc",
+                alt_bg = false,
+                cursorline_gutter = false,
+                dark_gutter = false,
+                plain_float = true,
+                transparent = false,
+                code_style = {
+                    headings = "italic",
+                },
+                plugin = {
+                    lualine = {
+                        plain = false,
+                    },
+                    cmp = {
+                        plain = true,
+                    },
+                },
+                highlights = {
+                    ["LazyNormal"] = { bg = "none" },
+                    ["Statusline"] = { bg = "$alt_bg" },
+                    ["StatuslineTerm"] = { bg = "$alt_bg" },
+                    ["FloatBorder"] = { fg = "$operator" },
+                    ["WinSeparator"] = { fg = "$operator" },
+                },
+            })
+            require("neomodern").load()
+        end,
+    },
 }
