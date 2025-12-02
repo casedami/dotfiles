db096aa191f561611d14f3e1ffe7d262791b723c
Casey Miller <cd.miller1230@gmail.com>
2025-12-01 21:34:26 -0700

    feat(nvim): fix gitsigns

--- nvim/.config/nvim/plugin/session.lua
+++ nvim/.config/nvim/plugin/session.lua
@@ -1,20 +1,20 @@
 vim.api.nvim_create_user_command("SessionSave", function()
-    local M = require("session")
+    local M = require("self.session")
     M:try_save()
 end, { nargs = 0 })
 
 vim.api.nvim_create_user_command("SessionList", function()
-    local M = require("session")
+    local M = require("self.session")
     M:list()
 end, { nargs = 0 })
 
 vim.api.nvim_create_user_command("SessionLoad", function()
-    local M = require("session")
+    local M = require("self.session")
     M:try_source()
 end, { nargs = 0 })
 
 vim.api.nvim_create_user_command("SessionDelete", function(opts)
-    local M = require("session")
+    local M = require("self.session")
     M:try_delete(opts.args)
 end, { nargs = "*" })
 
