dbf0e6394036611d91a91eecd33889a5ad62ec9b
Casey Miller <cd.miller1230@gmail.com>
2025-12-03 15:50:45 -0700

    wip

--- nvim/.config/nvim/plugin/session.lua
+++ nvim/.config/nvim/plugin/session.lua
@@ -1,22 +1,22 @@
-vim.api.nvim_create_user_command("SessionSave", function()
-    local M = require("self.session.plugin")
-    M:save()
-end, { nargs = 0 })
+vim.api.nvim_create_user_command("SessionSave", function(opts)
+    require("self.session.plugin").save(opts.args)
+end, { nargs = "*" })
 
 vim.api.nvim_create_user_command("SessionList", function()
-    local M = require("self.session.plugin")
-    M:list()
+    require("self.session.plugin").list()
 end, { nargs = 0 })
 
 vim.api.nvim_create_user_command("SessionLoad", function()
-    local M = require("self.session.plugin")
-    M:select()
+    require("self.session.plugin").select()
 end, { nargs = 0 })
 
 vim.api.nvim_create_user_command("SessionDelete", function(opts)
-    local M = require("self.session.plugin")
-    M:try_delete(opts.args)
+    require("self.session.plugin").try_delete(opts.args)
 end, { nargs = "*" })
+
+vim.api.nvim_create_user_command("SessionSync", function()
+    require("self.session.plugin").sync()
+end, { nargs = 0 })
 
 vim.keymap.set("c", "SS", "SessionSave")
 vim.keymap.set("c", "SL", "SessionLoad")
