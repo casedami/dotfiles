798a10aa50225ccbac281a5b10ce0f7e4574db26
Casey Miller <cd.miller1230@gmail.com>
2025-12-10 14:35:22 -0700

    fix(nvim): simplify netrw opts

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -4,8 +4,6 @@ vim.g.maplocalleader = "\\"
 
 -- Netrw
 vim.g.netrw_banner = 0
-vim.g.netrw_browse_split = 0
-vim.g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
 
 -- Search
 vim.opt.ignorecase = true
