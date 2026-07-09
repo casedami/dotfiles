14c2c62ff83647d024693dead4a4e81449bfa89f
Casey Miller <cd.miller1230@gmail.com>
2026-07-09 10:29:19 -0600

    add keyboard config

---
+++ keyboard/flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "vial-qmk build environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          qmk
+          gcc-arm-embedded
+          git
+        ];
+        shellHook = ''
+          export QMK_HOME=$(pwd)
+        '';
+      };
+    };
+}
